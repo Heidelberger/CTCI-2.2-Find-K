e5019dce5959165689fb0c390f366d751fb21a39
Qubert <upafourth@gmail.com>
2017-12-06 10:53:09 +0100

    Add project files.

---
+++ CTCI 2.2 Find K/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_2._2_Find_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
